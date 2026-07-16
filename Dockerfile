9e6f239127bd184d046c252ca30afcaa9d46635d
nightseven95 <nightseven95@local.ai>
2026-07-16 10:50:57 +0000

    update codebase directory hierarchy

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.14
 
+ARG EXECUTOR_TYPE="shufersal/collectors/store"
 COPY . /app
 WORKDIR /app
 RUN ./scripts/init.sh
-CMD ["python3", "main.py"]
+CMD ["python3", "main.py", "${EXECUTOR_TYPE}/main.py"]
--- src/shufersal/collectors/stores/main.py
+++ src/shufersal/collectors/stores/main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import gzip
 import io
 from pathlib import Path
+import asyncio
 
 
 def default_html_parser(text):
@@ -18,12 +19,14 @@ def default_http_getter(*args, **kwargs):
 def default_gzip_decompressor(data):
     file_object = io.BytesIO(data)
     with gzip.GzipFile(fileobj=file_object) as decompressor:
-        decompressed_data = decompressor.read().decode('utf-8')
+        decompressed_data = decompressor.read().decode("utf-8")
     return decompressed_data
+
 
 def default_directory_creator(directory_path):
     dir_path_path = Path(directory_path)
     dir_path_path.mkdir(parents=True, exist_ok=True)
+
 
 class StoreStorage:
     def __init__(self, directory_creator, base_directory):
@@ -31,18 +34,21 @@ class StoreStorage:
         directory_creator(self._base_directory)
 
     def save(self, filename, content):
-        with open('/'.join([self._base_directory, filename]), 'w', encoding='utf-8') as file:
+        with open(
+            "/".join([self._base_directory, filename]), "w", encoding="utf-8"
+        ) as file:
             file.write(content)
 
 
 def extract_filename_from_url(url):
     try:
-        clean_url = url.split('?')[0]
-        filename_with_ext = clean_url.split('/')[-1]
-        filename = filename_with_ext.split('.gz')[0]
+        clean_url = url.split("?")[0]
+        filename_with_ext = clean_url.split("/")[-1]
+        filename = filename_with_ext.split(".gz")[0]
         return filename if filename else None
     except (IndexError, AttributeError):
         return None
+
 
 def get_stores_xml_urls(base_url, **kwargs):
     path = kwargs.get("path", "")
@@ -62,11 +68,14 @@ def get_stores_xml_urls(base_url, **kwargs):
     return urls
 
 
-def get_stores_info_xml(url, **kwargs):
+async def get_stores_info_xml(url, **kwargs):
+    event_loop = kwargs.get("event_loop", asyncio.get_event_loop())
     http_getter = kwargs.get("http_getter", default_http_getter)
     decompressor = kwargs.get("gzip_decompressor", default_gzip_decompressor)
 
-    response = http_getter(url, stream=True)
+    response = await event_loop.run_in_executor(
+        None, http_getter, url, dict(stream=True)
+    )
     response.raise_for_status()
 
     filename = extract_filename_from_url(url)
@@ -74,16 +83,16 @@ def get_stores_info_xml(url, **kwargs):
     return (filename, decompressor(response.content))
 
 
-def main(base_url, path, params, store_storage):
+async def main(base_url, path, params, store_storage):
     urls = get_stores_xml_urls(base_url, path=path, params=params)
     for url in urls:
-        filename, data = get_stores_info_xml(url)
+        filename, data = await get_stores_info_xml(url)
         store_storage.save(filename, data)
 
-print(__name__)
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     base_url = "https://prices.shufersal.co.il"
     path = "FileObject/UpdateCategory"
     params = dict(catID=5, storeId=0)
     store_storage = StoreStorage(default_directory_creator, "./raw/stores")
-    main(base_url, path, params, store_storage)
+    asyncio.run(main(base_url, path, params, store_storage))
